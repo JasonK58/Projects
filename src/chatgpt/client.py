4352f6d0ffb524deed3f21205cccaefac5f94c95
JasonK58 <JasonK58@gmail.com>
2025-01-14 11:06:09 -0800

    Feature/add client (#7)
    * add chatGPT client and test
    
    * update test docstring
    
    * disbale single pylint warning

---
+++ src/chatgpt/client.py
@@ -0,0 +1,31 @@
+"""
+ChatGPT Client class
+"""
+
+import os
+import string
+
+from openai import OpenAI
+
+MODEL = "gpt-4o"
+ROLE = "user"
+
+
+class ChatGptClient:  # pylint: disable=too-few-public-methods
+    """
+    ChatGPT client to make server requests
+    """
+
+    def __init__(self):
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    def make_request(self, content_message: string):
+        """
+        Makes a request to ChatGPT server
+        :return: Generated AI response
+        """
+        chat_completion = self.client.chat.completions.create(
+            messages=[{"role": ROLE, "content": content_message}], model=MODEL
+        )
+
+        return chat_completion
---
+++ tests/chatgpt/test_client.py
@@ -0,0 +1,42 @@
+"""
+Unit Tests for ChatGPT client
+"""
+
+import unittest
+from unittest import mock
+from unittest.mock import MagicMock
+
+from src.chatgpt.client import ChatGptClient
+
+
+class TestChatGptClient(unittest.TestCase):
+    """
+    ChatGPT client tests
+    """
+
+    @mock.patch("os.environ", {"OPENAI_API_KEY": "mocked_key"})
+    @mock.patch("src.chatgpt.client.OpenAI")
+    def test_make_request(self, mock_open_ai):
+        """
+        Test a request can be made to the client.
+        """
+        # Arrange
+        content_message = "What is 1 + 1?"
+        response_message = "The answer is 2."
+        mock_openai_instance = mock_open_ai.return_value
+
+        mock_chat_completions_create = MagicMock()
+        mock_chat_completions_create.return_value = {
+            "choices": [{"message": {"content": response_message}}]
+        }
+
+        mock_openai_instance.chat.completions.create = mock_chat_completions_create
+
+        client = ChatGptClient()
+
+        # Act
+        response = client.make_request(content_message)
+        result = response["choices"][0]["message"]["content"]
+
+        # Assert
+        self.assertEqual(result, response_message)
