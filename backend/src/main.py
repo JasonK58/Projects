3cb2f39573243d532588c7580b57033558574d08
Jason Kolenosky <JasonK58@gmail.com>
2025-02-25 15:25:25 -0800

    added cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,10 +3,23 @@ Project entrypoint
 """
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .routers import home, writingprompt
 
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
+
+origins = [
+    "http://localhost/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(home.home_router)
 app.include_router(writingprompt.writing_prompt_router)
