8e33aa08b6b7409dbe202847363f18396667d819
Jason Kolenosky <JasonK58@gmail.com>
2025-02-25 17:32:23 -0800

    add api composable and ignore env

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,11 +4,14 @@ import Logo from '@/assets/books.svg'
 import DropdownMenu from '@/components/dropdownMenu.vue'
 import { ref } from 'vue'
 import TextInput from '@/components/TextInput.vue'
+import { useApi } from '@/composables/useApi.ts'
 
 const maxLength = 20
 const maxInputs = 5
+const apiUrl = import.meta.env.VITE_API_URL
 const selectedGenre = ref('')
 const textInputs = ref([''])
+const { makePostRequest, results, error } = useApi()
 </script>
 
 <template>
@@ -21,4 +24,18 @@ const textInputs = ref([''])
   />
 
   <TextInput :max-length="maxLength" :max-inputs="maxInputs" v-model="textInputs" />
+
+  <button
+    @click="
+      makePostRequest(apiUrl + 'writingprompt', {
+        genre: selectedGenre,
+        keywords: textInputs,
+      })
+    "
+  >
+    Generate Writing Prompt</button
+  ><br />
+  <span>Result: {{ results }}</span
+  ><br />
+  <span>Error: {{ error }}</span>
 </template>
