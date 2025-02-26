0a41cb0bc1f7ca87997acf2d5c33829daaa59884
Jason Kolenosky <JasonK58@gmail.com>
2025-02-25 17:23:53 -0800

    Revert "added composable for api call"
    This reverts commit 2713f38add817bd5cbd71668d8d4f02262ca013f.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,14 +4,11 @@ import Logo from '@/assets/books.svg'
 import DropdownMenu from '@/components/dropdownMenu.vue'
 import { ref } from 'vue'
 import TextInput from '@/components/TextInput.vue'
-import { useApi } from '@/composables/useApi.ts'
 
 const maxLength = 20
 const maxInputs = 5
-const apiUrl = import.meta.env.VITE_API_URL
 const selectedGenre = ref('')
 const textInputs = ref([''])
-const { makePostRequest, results, error } = useApi()
 </script>
 
 <template>
@@ -24,18 +21,4 @@ const { makePostRequest, results, error } = useApi()
   />
 
   <TextInput :max-length="maxLength" :max-inputs="maxInputs" v-model="textInputs" />
-
-  <button
-    @click="
-      makePostRequest(apiUrl + 'writingprompt', {
-        genre: selectedGenre,
-        keywords: textInputs,
-      })
-    "
-  >
-    Generate Writing Prompt</button
-  ><br />
-  <span>Result: {{ results }}</span
-  ><br />
-  <span>Error: {{ error }}</span>
 </template>
