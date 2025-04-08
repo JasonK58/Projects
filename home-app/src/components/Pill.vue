97eebfbd2f7d00904ea4119dc2b81965ac5fba8b
JasonK58 <JasonK58@gmail.com>
2025-04-08 15:50:59 -0700

    created a home app as base of other projects (#30)

---
+++ home-app/src/components/Pill.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  url: string;
+}>();
+</script>
+
+<template>
+  <div class="pill">
+    <a :href="url" target="_blank" class="title">{{ title }}</a>
+  </div>
+</template>
+
+<style scoped>
+.pill {
+  display: inline-flex;
+  border: 3px solid black;
+  border-radius: 5px;
+  background-color: #f79e2f;
+}
+
+.pill:hover {
+  cursor: pointer;
+}
+
+.title {
+  padding: 7px;
+  font-weight: 550;
+  text-decoration: none;
+  color: black;
+}
+</style>
---
+++ home-app/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Heading from "../components/Heading.vue";
+import AppHeader from "../components/AppHeader.vue";
+import Pill from "../components/Pill.vue";
+</script>
+
+<template>
+  <AppHeader />
+  <Heading title="Projects" />
+  <Pill title="Writing Prompt Generator" url="/writing-prompts" />
+</template>
+
+<style scoped></style>
