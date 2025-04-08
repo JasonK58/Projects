4dbd837d96641f4abf984c096f1956d37e03790d
Jason Kolenosky <JasonK58@gmail.com>
2025-04-08 15:49:32 -0700

    created a home app as base of other projects

---
+++ home-app/src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <header>Jason Kolenosky</header>
+</template>
+
+<style scoped>
+header {
+  font-size: 4rem;
+  text-align: center;
+  background-color: #f6dea0;
+}
+
+@media screen and (max-width: 991px) {
+  header {
+    font-size: 3rem;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  header {
+    font-size: 2rem;
+  }
+}
+
+@media screen and (max-width: 479px) {
+  header {
+    font-size: 1rem;
+  }
+}
+</style>
---
+++ home-app/src/components/Heading.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="heading-title">{{ title }}</div>
+</template>
+
+<style scoped>
+.heading-title {
+  font-size: 2rem;
+  width: 100%;
+  padding: 1rem 0 1rem 0;
+}
+</style>
